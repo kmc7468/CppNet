472aad0ad8215af8193ce561d9ccfc5ba495a191
PHJ-DESKTOP\led78 <led789zxpp@naver.com>
2016-09-07 19:43:06 +0900

    타입 얼라이스 추가 콘솔 일부 구현

--- CSClasses/CSClasses/Console.h
+++ CSClasses/CSClasses/Console.h
@@ -8,22 +8,77 @@ namespace System
 	class Console final
 	{
 	public:
+		Console() = default;
+		Console(Console &&sNewConsole) = delete;
+		Console(const Console &sNewConsole) = delete;
+		~Console() = default;
+
+	public:
+		Console &operator=(Console &&sNewConsole) = delete;
+		Console &operator=(const Console &sNewConsole) = delete;
+
+	public:
+		inline void Write(Boolean boolean)
+		{
+			std::wcout << boolean;
+		}
+
+		inline void Write(Char character) const
+		{
+			std::wcout << character;
+		}
+
+		inline void Write(const Char *string) const
+		{
+			std::wcout << string;
+		}
+
+		inline void Write(const Char *string, Int32 index, Int32 count)
+		{
+			std::wcout.write(string + index, count);
+		}
+
+		inline void Write(Double doubled) const
+		{
+			std::wcout << doubled;
+		}
+
+		inline void Write(Int32 integer) const
+		{
+			std::cout << integer;
+		}
+
+		inline void Write(Int64 integer) const
+		{
+			std::cout << integer;
+		}
+
+		inline void Write(UInt32 integer) const
+		{
+			std::cout << integer;
+		}
+
+		inline void Write(UInt64 integer) const
+		{
+			std::cout << integer;
+		}
+
+		inline void WriteLine() const
+		{
+			std::cout << std::endl;
+		}
+
+		inline void WriteLine(Boolean boolean) const
+		{
+			std::cout << std::endl;
+		}
+
 		inline void WriteLine(const char* string) const
 		{
 			std::cout << string << std::endl;
 		}
 
 		inline void WriteLine(int integer) const
-		{
-			std::cout << integer << std::endl;
-		}
-
-		inline void Write(const char* string) const
-		{
-			std::cout << string;
-		}
-
-		inline void Write(int integer) const
 		{
 			std::cout << integer << std::endl;
 		}
--- CSClasses/CSClasses/System.h
+++ CSClasses/CSClasses/System.h
@@ -1,16 +1,24 @@
+
 #ifndef SYSTEM_H
+
 #define SYSTEM_H
 
-typedef unsigned char Byte;
-typedef signed char SByte;
-typedef unsigned short UInt16;
-typedef unsigned int UInt32;
-typedef unsigned long long UInt64;
-typedef short Int16;
-typedef int Int32;
-typedef int Int64;
-typedef double Double;
-typedef float Single;
+using Boolean = bool;
+using Char = wchar_t;
+
+using Byte = unsigned char;
+using SByte = signed char;
+
+using Int16 = short;
+using Int32 = int;
+using Int64 = long long;
+
+using UInt16 = unsigned short;
+using UInt32 = unsigned int;
+using UInt64 = unsigned long long;
+
+using Single = float;
+using Double = double;
 
 #include "Console.h"
 #include "Math.h"
