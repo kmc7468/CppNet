0e96dc5b2fce43af518bfbcd885ce01954fe411b
kmc7468 <kmc7468@naver.com>
2016-09-08 19:17:02 +0900

    ExceptionData 생성자 개선

--- CSClasses/CSClasses/Exception.h
+++ CSClasses/CSClasses/Exception.h
@@ -11,7 +11,7 @@ namespace System
 		class ExceptionData
 		{
 		public:
-			explicit ExceptionData(const String& message, const String& source, const String& link, void* target, Exception* inner)
+			explicit ExceptionData(const String& message = L"", const String& source = L"", const String& link = L"", void* target = nullptr, Exception* inner = nullptr)
 			{
 				this->message = message;
 				this->source = source;
