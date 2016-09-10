d18acc12f0c65ad023d44f7a5f98a1c020c6f5f3
kmc7468 <kmc7468@naver.com>
2016-09-10 19:24:00 +0900

    Console, Exception, Convert : 헤더파일 및 소스파일 분리

--- CSClasses/CSClasses/Exception.h
+++ CSClasses/CSClasses/Exception.h
@@ -11,43 +11,14 @@ namespace System
 		class ExceptionData
 		{
 		public:
-			ExceptionData(const String& message = L"", const String& source = L"", const String& link = L"", void* target = nullptr, Exception* inner = nullptr)
-			{
-				this->message = message;
-				this->source = source;
-				this->link = link;
-				this->target = target;
-				this->inner = inner;
-			}
-			ExceptionData(ExceptionData &&sNewExceptionData)
-			{
-				message = sNewExceptionData.message;
-				source = sNewExceptionData.source;
-				link = sNewExceptionData.link;
-				target = sNewExceptionData.target;
-				inner = sNewExceptionData.inner;
-				// FIXME inner 얕은복사
-			}
-			ExceptionData(const ExceptionData& sNewExceptionData)
-			{
-				message = sNewExceptionData.message;
-				source = sNewExceptionData.source;
-				link = sNewExceptionData.link;
-				target = sNewExceptionData.target;
-				inner = sNewExceptionData.inner;
-				// FIXME inner 얕은복사
-			}
+			ExceptionData(const String& message = L"", const String& source = L"", const String& link = L"", void* target = nullptr, Exception* inner = nullptr);
+			ExceptionData(ExceptionData &&sNewExceptionData);
+			ExceptionData(const ExceptionData& sNewExceptionData);
 			virtual ~ExceptionData() = default;
 
 		public:
-			ExceptionData& operator=(const ExceptionData& ex)
-			{
-				return ExceptionData(ex);
-			}
-			ExceptionData& operator=(ExceptionData&& ex)
-			{
-				return ExceptionData(ex);
-			}
+			ExceptionData& operator=(const ExceptionData& ex);
+			ExceptionData& operator=(ExceptionData&& ex);
 
 		private:
 			String message;
@@ -57,69 +28,20 @@ namespace System
 			Exception* inner;
 
 		public:
-			String Message() const
-			{
-				return message;
-			}
-			 
-			String HelpLink() const
-			{
-				return link;
-			}
-
-			String Source() const
-			{
-				return source;
-			}
-
-			void* TargetSite() const
-			{
-				return target;
-			}
-
-			Exception* InnerException() const
-			{
-				return inner;
-			}
+			String Message() const;
+			String HelpLink() const;
+			String Source() const;
+			void* TargetSite() const;
+			Exception* InnerException() const;
 		};
 
 	public:
 		Exception() = default;
-		Exception(const String& message)
-		{
-			this->message = message;
-		}
-		Exception(const String& message, Exception* innerEx)
-		{
-			this->message = message;
-			innerException = innerEx;
-		}
-		Exception(const ExceptionData& data)
-		{
-			this->message = data.Message();
-			this->source = data.Source();
-			this->targetsite = data.TargetSite();
-			this->link = data.HelpLink();
-			this->innerException = data.InnerException();
-		}
-		Exception(Exception &&sNewException)
-		{
-			message = sNewException.message;
-			source = sNewException.source;
-			link = sNewException.link;
-			targetsite = sNewException.targetsite;
-			innerException = sNewException.innerException;
-			// FIXME innerException 얕은복사
-		}
-		Exception(const Exception& sNewException)
-		{
-			message = sNewException.message;
-			source = sNewException.source;
-			link = sNewException.link;
-			targetsite = sNewException.targetsite;
-			innerException = sNewException.innerException;
-			// FIXME innerException 얕은복사
-		}
+		Exception(const String& message);
+		Exception(const String& message, Exception* innerEx);
+		Exception(const ExceptionData& data);
+		Exception(Exception &&sNewException);
+		Exception(const Exception& sNewException);
 		virtual ~Exception() = default;
 
 	protected:
@@ -130,45 +52,15 @@ namespace System
 		String link;
 
 	public:
-		Exception& operator=(const Exception& ex)
-		{
-			return Exception(ex);
-		}
-		Exception& operator=(Exception&& ex)
-		{
-			return Exception(ex);
-		}
+		Exception& operator=(const Exception& ex);
+		Exception& operator=(Exception&& ex);
 
 	public:
-		virtual String Message() const
-		{
-			String msg = TXT_EXCEPTION_DEFMSG;
-			msg.append(L" ");
-			msg.append(message);
-
-			return msg;
-		}
-
-		virtual const void* const TargetSite() const
-		{
-			return targetsite;
-		}
-
-		virtual String* Source()
-		{
-			String* s = &source;
-			return s;
-		}
-
-		virtual const Exception* const InnerExcpetion()
-		{
-			return innerException;
-		}
-
-		virtual String HelpLink()
-		{
-			return link;
-		}
+		virtual String Message() const;
+		virtual const void* const TargetSite() const;
+		virtual String* Source();
+		virtual const Exception* const InnerExcpetion() const;
+		virtual String HelpLink() const;
 
 		// NOTE: 구현하지 않음- StackFrace, HResult
 	};
