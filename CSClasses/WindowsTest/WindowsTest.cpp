76e34687c7ba74f56fe3c2a66697704f8cb31001
kmc7468 <kmc7468@naver.com>
2016-09-15 18:10:19 +0900

    WindowsTest Project Added

---
+++ CSClasses/WindowsTest/WindowsTest.cpp
@@ -0,0 +1,56 @@
+// SourceURL: http://mrhook.co.kr/89
+
+#include "stdafx.h"
+
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("first");
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	switch (iMessage)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	case WM_LBUTTONDOWN:
+		MessageBeep(0);     // 마우스 버튼 클릭시 "띵띵" 소리가 나온다.
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
+
+
+int WINAPI WinMain(HINSTANCE hlnstance,
+	HINSTANCE hPrevInstance, LPSTR IpCmdLine, int nCmdShow) {
+
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hlnstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(COLOR_WINDOW + 1);
+
+	WndClass.hCursor = LoadCursor(NULL, IDC_NO);
+	WndClass.hIcon = LoadIcon(NULL, IDI_ERROR);
+
+	WndClass.hInstance = hlnstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hlnstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0))
+	{
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+
+	return (int)Message.wParam;
+}
