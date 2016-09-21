c058e2d9852c936ff07df863e0e244ff5fb06114
kmc7468 <kmc7468@naver.com>
2016-09-21 23:31:27 +0900

    CharUnicodeInfo 좀더 구현

--- CSClasses/CSClasses/CharUnicodeInfo.cpp
+++ CSClasses/CSClasses/CharUnicodeInfo.cpp
@@ -9,6 +9,50 @@ Int32 CharUnicodeInfo::GetDecimalDightValue(const Char& c)
 	{
 		return cc - U'0';
 	}
+	else if (cc >= U'\u0660' && cc <= U'\u0669')
+	{
+		return cc - U'\u0660';
+	}
+	else if (cc >= U'\u06F0' && cc <= U'\u06F9')
+	{
+		return cc - U'\u06F0';
+	}
+	else if (cc >= U'\u07C0' && cc <= U'\u07C9')
+	{
+		return cc - U'\u07C0';
+	}
+	else if (cc >= U'\u0966' && cc <= U'\u096F')
+	{
+		return cc - U'\u0966';
+	}
+	else if (cc >= U'\u09E6' && cc <= U'\u09EF')
+	{
+		return cc - U'\u09EF';
+	}
+	else if (cc >= U'\u0A66' && cc <= U'\u0A6F')
+	{
+		return cc - U'\u0A66';
+	}
+	else if (cc >= U'\u0AE6' && cc <= U'\u0AEF')
+	{
+		return cc - U'\u0AE6';
+	}
+	else if (cc >= U'\u0B66' && cc <= U'\u0B6F')
+	{
+		return cc - U'\u0B66';
+	}
+	else if (cc >= U'\u0BE6' && cc <= U'\u0BEF')
+	{
+		return cc - U'\u0BE6';
+	}
+	else if (cc >= U'\u0C66' && cc <= U'\u0C6F')
+	{
+		return cc - U'\u0C66';
+	}
+	else if (cc >= U'\u0CE6' && cc <= U'\u0CEF')
+	{
+		return cc - U'\u0CE6';
+	}
 	// TODO
-	return 0;
+	return -1;
 }
