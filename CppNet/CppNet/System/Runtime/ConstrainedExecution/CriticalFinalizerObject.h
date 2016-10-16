e6c243e28b64a54aeeadbf7a71ad0d5c08d89e38
static <kmc7468@naver.com>
2016-10-16 21:31:53 +0900

    System.Runtime.ConstrainedExecution

---
+++ CppNet/CppNet/System/Runtime/ConstrainedExecution/CriticalFinalizerObject.h
@@ -0,0 +1,14 @@
+#ifndef SYSTEM_RUNTIME_CONSTRAINEDEXECUTION_CRITICALFINALIZEROBJECT_H
+#define SYSTEM_RUNTIME_CONSTRAINEDEXECUTION_CRITICALFINALIZEROBJECT_H
+
+#include "../../Object.h"
+
+namespace System::Runtime::ConstrainedExecution
+{
+	class CriticalFinalizerObject : public Object
+	{
+
+	};
+}
+
+#endif
