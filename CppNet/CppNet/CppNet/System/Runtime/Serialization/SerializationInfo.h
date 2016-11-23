73da3ffe2bd6de53b6e3edc881df4ad2fbeb411e
static <kmc7468@naver.com>
2016-11-23 23:17:39 +0900

    SerializationInfo.h 작업

--- CppNet/CppNet/CppNet/System/Runtime/Serialization/SerializationInfo.h
+++ CppNet/CppNet/CppNet/System/Runtime/Serialization/SerializationInfo.h
@@ -8,6 +8,7 @@
 #include "../../Int32.h"
 #include "../../String.h"
 #include "../../Type.h"
+#include "../../ArgumentNullException.hpp"
 
 #include "../../Collections/Generic/Dictionary.h"
 
@@ -39,7 +40,56 @@ namespace CppNet
 					IFormatterConverter* m_converter = nullptr;
 
 				private:
+					String m_fullTypeName;
+					Type* objectType = nullptr;
+					Boolean isFullTypeNameSetExplicit;
+					Boolean requireSameTokenInPartialTrust;
 
+
+				public:
+					SerializationInfo(Type* type, IFormatterConverter* converter)
+						: SerializationInfo(type, converter, false)
+					{}
+
+					SerializationInfo(Type* type, IFormatterConverter* converter, Boolean requireSameTokenInPartialTrust)
+					{
+						if (type == nullptr)
+							throw ArgumentNullException(nameof(type));
+						if (converter == nullptr)
+							throw ArgumentNullException(nameof(converter));
+
+						objectType = type;
+						m_fullTypeName = type->name();
+
+						m_members = new String[defaultSize];
+						m_data = new Object[defaultSize];
+						m_types = (Type*)(new Byte[defaultSize * sizeof(Type)]);
+
+						m_converter = converter;
+
+						this->requireSameTokenInPartialTrust = requireSameTokenInPartialTrust;
+					}
+
+					String GetFullTypeName() const
+					{
+						return m_fullTypeName;
+					}
+
+					void SetFullTypeName(const String& value)
+					{
+						m_fullTypeName = value;
+						isFullTypeNameSetExplicit = true;
+					}
+
+					void SetType(Type* type)
+					{
+						if (type == nullptr)
+							throw ArgumentNullException(nameof(type));
+
+						// TODO
+					}
+					
+					// TODO
 				};
 			}
 		}
