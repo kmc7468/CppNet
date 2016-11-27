b8052833e48733143e44a54ad393b4b427e08a8e
kmc7468 <kmc7468@naver.com>
2016-11-27 12:17:45 +0900

    .

--- CppNet/Test/main.cpp
+++ CppNet/Test/main.cpp
@@ -8,13 +8,7 @@ int main()
 	using namespace CppNet::System;
 	using namespace CppNet::KMC;
 
-	Sor16 s(L"’×");
-
-	auto a = s.Encrypt(L"wwww", 27);
-	auto b = s.Decrypt(std::get<0>(a), 27);
-
-	std::wcout << std::wstring(std::get<0>(a), std::get<1>(a)) << std::endl;
-	std::wcout << std::wstring(std::get<0>(b), std::get<1>(b)) << std::endl;
+	// TEST CODE INPUT
 
 	MAIN_END
 }
